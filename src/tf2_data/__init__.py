b24e5d0be4d32ea7b4c8b2f23a5ff3029bde0716
offish <overutilization@gmail.com>
2025-12-10 19:39:45 +0100

    add smissmas items

--- src/tf2_data/__init__.py
+++ src/tf2_data/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 __title__ = "tf2-data"
 __author__ = "offish"
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 __license__ = "MIT"
 
 from .constants import COLORS, EXTERIORS, KILLSTREAKS, QUALITIES, QUALITY_COLORS, WEARS
